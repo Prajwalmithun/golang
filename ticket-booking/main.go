c40e92898f186719a66d714e3f2079c19d82302a
Prajwalmithun <tkushal216@gmail.com>
2022-03-14 23:51:08 +0530

    day2

---
+++ ticket-booking/main.go
@@ -0,0 +1,9 @@
+package main   //"main" just a standard way, but it can be anything
+
+import "fmt"  //fmt is builtin package for formating, like displaying output to std output 
+
+// entry point to the complier
+func main()  {
+	fmt.Println("Hello World from Golang")
+	
+}
